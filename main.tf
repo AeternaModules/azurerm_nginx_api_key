3da0631bf043c4f62134f6cfe1040edadfc8a186
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:59:02 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
+data "azurerm_key_vault_secret" "secret_text" {
+  for_each     = { for k, v in var.nginx_api_keys : k => v if v.secret_text_key_vault_id != null && v.secret_text_key_vault_secret_name != null }
+  name         = each.value.secret_text_key_vault_secret_name
+  key_vault_id = each.value.secret_text_key_vault_id
+}
 resource "azurerm_nginx_api_key" "nginx_api_keys" {
   for_each = var.nginx_api_keys
 
   end_date_time       = each.value.end_date_time
   name                = each.value.name
   nginx_deployment_id = each.value.nginx_deployment_id
-  secret_text         = each.value.secret_text
+  secret_text         = each.value.secret_text != null ? each.value.secret_text : try(data.azurerm_key_vault_secret.secret_text[each.key].value, null)
 }
 
--- variables.tf
+++ variables.tf
@@ -6,13 +6,17 @@ Required:
     - name
     - nginx_deployment_id
     - secret_text
+    - secret_text_key_vault_id (alternative to secret_text - read from Key Vault instead)
+    - secret_text_key_vault_secret_name (alternative to secret_text - read from Key Vault instead)
 EOT
 
   type = map(object({
-    end_date_time       = string
-    name                = string
-    nginx_deployment_id = string
-    secret_text         = string
+    end_date_time                     = string
+    name                              = string
+    nginx_deployment_id               = string
+    secret_text                       = string
+    secret_text_key_vault_id          = optional(string)
+    secret_text_key_vault_secret_name = optional(string)
   }))
 }
 
